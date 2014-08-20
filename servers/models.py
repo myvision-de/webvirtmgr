e58bfae2cab9562f04e785abd2632f1c3d91613d
Philipp Steinhardt <steinhardt.philipp@gmail.com>
2014-08-20 14:46:33 +0200

    set max_length for varchar columns on server model to 255 chars

--- servers/models.py
+++ servers/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Compute(models.Model):
-    name = models.CharField(max_length=20)
-    hostname = models.CharField(max_length=20)
-    login = models.CharField(max_length=20)
-    password = models.CharField(max_length=14, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    hostname = models.CharField(max_length=255)
+    login = models.CharField(max_length=255)
+    password = models.CharField(max_length=255, blank=True, null=True)
     type = models.IntegerField()
 
     def __unicode__(self):
